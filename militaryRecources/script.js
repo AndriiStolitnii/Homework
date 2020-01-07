6e8726a40ab817783d9a02e44a213c9b41bb68a4
Andrii_Stolitnii <a.s.stoletniy@gmail.com>
2020-01-07 16:01:51 +0200

    Military Recourcesis ready

---
+++ militaryRecources/script.js
@@ -0,0 +1,29 @@
+var horseman = new MilitaryResource('horseman', 300, 800);
+// console.log(horseman);
+var warrior = new MilitaryResource('warrior', 200, 500);
+// console.log(warrior.isReadyToMove());
+var archer = new MilitaryResource('archer', 150, 600);
+// console.log(archer.isReadyToFight());
+var horsemanClone = horseman.clone();
+// console.log(horsemanClone);
+archer.health = 0;
+// console.log(archer);
+archer.restore();
+// console.log(archer);
+ 
+var squad = new Squad(warrior);
+squad.combineResources(horseman);
+// console.log(squad);
+
+var squadClone = squad.cloneResource();
+// console.log(squadClone, squadClone === squad);
+
+squad.combineResources(archer);
+// console.log(squad, squadClone);
+
+// console.log(squad.isReadyToMove());
+// console.log(squad.isReadyToFight());
+squad.squad[0].health = 0;
+squad.restore();
+squad.squad[1].distance = 0;
+squad.getReadyToMoveResources();
