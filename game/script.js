e71325604e05470e5d516d64360191fbac99c1df
Andrii_Stolitnii <a.s.stoletniy@gmail.com>
2020-01-12 18:23:19 +0200

    added attack

---
+++ game/script.js
@@ -0,0 +1,6 @@
+var shadow2 = new BlackShadow(2);
+console.log(shadow2);
+var player = new Player(1);
+console.log(player);
+player.attack(shadow2);
+console.log(player, shadow2);
--- game/unit.js
+++ game/unit.js
@@ -1,19 +1,13 @@
 var Unit = (function() {
     function Unit(baseDamage, health, level) {
-      this.el = document.createElement('div');
-      this.el.classList.add('cell__unit');
       var modifier = (level - 1) * 10;
       this.damage = baseDamage + modifier;
       this.maxHealth = this.health = health + modifier;
       this.level = level;
     }
   
-    Unit.prototype.attack = function() {
-      console.log('Attack', this);
-    };
-  
-    Unit.prototype.render = function() {
-      return this.el;
+    Unit.prototype.attack = function(monster) {
+      return this.health = this.health - monster.damage;
     };
   
     return Unit;
